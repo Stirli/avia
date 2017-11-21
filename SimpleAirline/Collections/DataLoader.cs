6f890a3b72aba30c5b44afc94434c7384f0e24e2
Дмитрий Владимирович Станский <d.v.stanski@outlook.com>
2017-11-21 06:57:50 +0300

    добавлено чтение

--- SimpleAirline/Collections/DataLoader.cs
+++ SimpleAirline/Collections/DataLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -13,8 +14,40 @@ namespace SimpleAirline
 
         public DataProvider()
         {
-            Tariffs = new HashSet<Tariff>();
-            Passangers = new HashSet<Passanger>();
+            try
+            {
+                Tariffs = new HashSet<Tariff>();
+                foreach (string line in File.ReadLines("tariffs.txt"))
+                {
+                    Tariffs.Add(ParseTariff(line));
+                }
+                Passangers = new HashSet<Passanger>();
+                foreach (string line in File.ReadLines("passangers.txt"))
+                {
+                    Passangers.Add(ParsePassanger(line));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Во время чтения данных произошла ошибка:" + e.Message, e);
+            }
+        }
+        // Парсит строку в тариф
+        private Tariff ParseTariff(string s)
+        {
+            string[] strings = s.Split(';');
+            return new Tariff(strings[0], strings[1], DateTime.Parse(strings[2]), double.Parse(strings[3]));
+        }
+
+        // Парсит строку в пассажира
+        private Passanger ParsePassanger(string s)
+        {
+            string[] strings = s.Split(';');
+            /*
+               Преобразование строки в Discount выполнено через оператор внутри класса Discount, а не при помощи метода
+               так как Discount - очень простая структура данных логически и физически
+             */
+            return new Passanger(strings[0], strings[1], (Discount)strings[2]);
         }
     }
 }
